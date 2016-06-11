16dfe6d03711dbded28a83c9c62eb878b5383138
xxx0624 <xzheng0624@gmail.com>
2016-06-11 20:52:05 +0800

    remove sth

--- sentence-similarity/semantic-lsa/test.py
+++
@@ -1,5 +0,0 @@
-from porter_stemer import PorterStemmer
-words = 'it\'s those dogs i liked'
-stem = PorterStemmer()
-print stem.stem(words, 0, len(words)-1)
-
--- sentence-similarity/stanford-parser-python-r22186/src/stanford_parser/my_start_parser.py
+++
@@ -1,5 +0,0 @@
-import my_main_parser
-
-parser = my_main_parser.main_parser()
-
-parser.main_solve(0.6, 0.2, 0.2, 0.1, 'msr_paraphrase_test.txt')
